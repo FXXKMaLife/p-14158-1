86d7f10427e065b293fd5ab0755ed5cc1913edfc
FXXKMaLife <lshine12393@gmail.com>
2026-05-06 22:50:45 +0900

    11강듣는중

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -16,7 +17,11 @@ public class BaseInitData {
         return args -> {
             System.out.println("기본 데이터가 초기화되었ㅅㅂ니다.");
 
-            postRepository.count(); //SELECT COUNT(*) FROM post;
+            //postRepository.count(); //SELECT COUNT(*) FROM post;
+            Post post = new Post();
+            post.setTitle("제목 1");
+
+            postRepository.save(post);//INSERT INTO post SET title = '제목 1';
         };
         //초기 데이터 삽입 로직을 여기에 작성
         //예 : 사용자, 카테고리, 태그 등 기본 데이터를 삽입
