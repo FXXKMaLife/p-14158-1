a173b9edab4aff0a8f23017db00a006bb2ec32f7
FXXKMaLife <lshine12393@gmail.com>
2026-05-07 23:36:29 +0900

    18강 Post에 붉은 밑줄

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,9 +1,11 @@
 package com.back.domain.post.post.entity;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -11,10 +13,12 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
 @Getter
 @Setter
-
+@RequiredArgsConstructor
 public class Post {
     @Id //primary key
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private int id; //INT
-    private String title; //VARCHAR(255)
+    private final String title; //VARCHAR(255)
+    @Column(columnDefinition = "TEXT")
+    private final String content;
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -17,16 +17,13 @@ public class BaseInitData {
         return args -> {
             if(postRepository.count() > 0) return;//데이터가 있는지 물어봄 있으면 Early return
 
+            /*  content를 추가했으니 컨텐트에 넣을것을 추가하자
             Post post1 = new Post();
             post1.setTitle("제목 1");
-
-            postRepository.save(post1);
-
-            //postRepository.count(); //SELECT COUNT(*) FROM post;
-            Post post2 = new Post();
-            post2.setTitle("제목 2");
-
-            postRepository.save(post2);//INSERT INTO post SET title = '제목 1';
+             postRepository.save(post1);
+             */
+            Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
+            Post post2 = postRepository.save(new Post("제목 2", "내용 2"));
 
             System.out.println("기본 데이터가 초기화되었ㅅㅂ니다.");
         };
