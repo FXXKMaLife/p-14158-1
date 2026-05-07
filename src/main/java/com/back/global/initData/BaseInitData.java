af63b413a354a9c1a665506138b917aa323281d4
FXXKMaLife <lshine12393@gmail.com>
2026-05-07 17:16:17 +0900

    12

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -15,13 +15,20 @@ public class BaseInitData {
     @Bean
     ApplicationRunner basinitDataApplicationRunner(){
         return args -> {
-            System.out.println("기본 데이터가 초기화되었ㅅㅂ니다.");
+            if(postRepository.count() > 0) return;//데이터가 있는지 물어봄 있으면 Early return
+
+            Post post1 = new Post();
+            post1.setTitle("제목 1");
+
+            postRepository.save(post1);
 
             //postRepository.count(); //SELECT COUNT(*) FROM post;
-            Post post = new Post();
-            post.setTitle("제목 1");
+            Post post2 = new Post();
+            post2.setTitle("제목 1");
 
-            postRepository.save(post);//INSERT INTO post SET title = '제목 1';
+            postRepository.save(post2);//INSERT INTO post SET title = '제목 1';
+
+            System.out.println("기본 데이터가 초기화되었ㅅㅂ니다.");
         };
         //초기 데이터 삽입 로직을 여기에 작성
         //예 : 사용자, 카테고리, 태그 등 기본 데이터를 삽입
